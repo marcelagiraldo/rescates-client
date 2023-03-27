4f7d952c340add736ffaecfaced15be0c8921e9f
yansaid21 <yansaid21@gmail.com>
2023-03-27 14:30:36 -0500

    primer menuTop

--- src/components/MenuTop/MenuTop.js
+++ src/components/MenuTop/MenuTop.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import "./MenuTop.scss"
+import React from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+import {Menu, Layout} from "antd"
+import { BellOutlined } from "@ant-design/icons"
+import { BarChartOutlined, BarsOutlined } from "@ant-design/icons/lib/icons"
 
-export const MenuTop = () => {
+export const MenuTop = (props) => {
+    const {Top} = Layout;
+    const location = useLocation();
+const navigate = useNavigate();
+
+    const menuItems=[
+        {
+            key:"/",
+            icon: <BellOutlined />,
+        },
+        {
+            key:"/",
+            icon: <BarChartOutlined />,
+        },
+        {
+            key:"/",
+            icon: <BarsOutlined />,
+        }
+    ]
+    const menuClick =(e)=>{
+        const path=e.path
+        console.log("di clic en el menu" + path);
+        navigate(path);
+
+    };
   return (
-    <div>MenuTop</div>
+    <Top className="menu-top" Collapsed ={props.menuCollapsed}>
+        <Menu
+        
+        mode="inline"
+    defaultSelectedKeys={[location.pathname]}
+    onClick= {menuClick}
+    items={menuItems}
+        />
+    </Top>
   )
 }
