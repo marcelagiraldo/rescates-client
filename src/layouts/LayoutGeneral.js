fb39b86d6ee367b1e4719208ecc9a3e80a2d0f60
marcelagiraldo <leidymarcelaalzategiraldo@gmail.com>
2023-03-31 12:09:49 -0500

    commit routes

--- src/layouts/LayoutGeneral.js
+++ src/layouts/LayoutGeneral.js
@@ -1,13 +1,15 @@
+/* Todo lo que esta comentado es lo del menu-top */
 import Layout from "antd/es/layout/layout";
-import React, { useState } from 'react'
-import { MenuTop } from '../components/MenuTop/MenuTop';
+import React/* , { useState }  */from 'react'
+/* import { MenuTop } from '../components/MenuTop/MenuTop'; */
 
 export const LayoutGeneral = (props) => {
     const {children} = props;
-    const [menuCollapsed, setMenuCollapsed] = useState(false);
+    /* const [menuCollapsed, setMenuCollapsed] = useState(false); */
     return (
         <Layout>
-            <MenuTop menuCollapsed = {menuCollapsed}/>
+            {/* <MenuTop menuCollapsed = {menuCollapsed}/> */}
+            <h1>Hola Mundo!</h1>
             {children}
         </Layout>
     )
